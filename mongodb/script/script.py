bdde437e1dfc827533337da80b97fa79f6470428
fulcus <francescofgonzales@gmail.com>
2021-11-30 00:23:33 +0100

    first working script all tests are in certificate

--- mongodb/script/script.py
+++ mongodb/script/script.py
@@ -5,8 +5,9 @@ import random
 
 def convert_to_GeoJSON(covid_centers):
     '''
-    Convert lat e long to GeoJSON object type
+    Convert lat e long to GeoJSON object type.
 
+    Refer to https://docs.mongodb.com/manual/reference/geojson/
 
     location: {
         type: "Point",
@@ -49,8 +50,7 @@ def main():
         random_person = random.sample(persons, 1)[0]
         person['emergency_contact'] = dict((k, random_person[k]) for k in ('first_name', 'last_name', 'phone_number', 'email'))
 
-
-    # Convert lat long to GeoJSON
+    # Convert lat and long fields to GeoJSON object type
     covid_centers = convert_to_GeoJSON(covid_centers)
 
     # get doctors from persons
@@ -70,20 +70,38 @@ def main():
         random_doctor = random.sample(doctors, 1)[0]
         vaccine['health_worker'] = dict((k, random_doctor[k]) for k in ('first_name', 'last_name', 'phone_number', 'email'))
 
-    # todo Add latest test to each person and associate vaccines to persons
-    
-    
-    # todo Add all vaccines to each person
+    # TODO Add latest test to each person and associate vaccines to persons
+    certificates = []
 
-    certificates = {
-        "certificates": persons,
+    for person in persons:
+        certificate = {}
+        certificate['person'] = person
+
+        # this should actually be saved in different collection
+        random_tests = random.sample(tests, random.randint(0, min(10, len(tests))))
+
+        certificate['tests'] = random_tests
+        for test in random_tests:
+            tests.remove(test)
+        
+        # Add all vaccines to each person
+        random_vaccines = random.sample(vaccines, random.randint(0, min(4, len(vaccines))))
+        certificate['vaccines'] = random_vaccines
+        for vaccine in random_vaccines:
+            vaccines.remove(vaccine)
+        
+        certificates.append(certificate)
+    
+
+    certificates_json = {
+        "certificates": certificates,
 
         #"tests_all": tests_all,
     }
 
     # Save the JSON files
     with open(cwd + '/data/certificates.json', 'w') as f:
-        json.dump(certificates, f)
+        json.dump(certificates_json, f)
 
 
 if __name__ == '__main__':
